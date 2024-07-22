5ef1bcd60bce42a47f399715bc944ab95dd1aaf2
Felipefigueiredo122 <felipefmoraes@icloud.com>
2024-07-22 18:11:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+x1 = float(input("Digite o primeiro numero: "))
+y1 = float(input("Digite o segundo numero: "))
+soma = x1 + y1
+print(f"A soma de {x1} + {y1} = {soma}")
+
+print("----------------------------------------------------------")
+
+x2 = float(input("Digtie o primeiro numero: "))
+y2 = float(input("Digite o segundo numero: "))
+sub= x2 - y2
+print(f"A subtraçã de {x2} - {y2} = {sub}")
+
+print("----------------------------------------------------------")
+
+x3= float(input("Digite o primeiro numero: "))
+y3= float(input("Digite o segundo numero: "))
+multi= x3*y3
+print(f"A multiplicação de {x3} * {y3} = {multi}")
+
+print("----------------------------------------------------------")
+x4 =float(input("Digite o primeiro numero: "))
+y4 =float(input("Digite o segundo numero: "))
+div= x4/y4
+print(f"A divisão de {x4} / {y4} = {div}")
